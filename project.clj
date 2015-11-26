cbd3891c69ee6a2a24467982df588e7648d9f730
Mike Ball <michaelhball@gmail.com>
2015-11-25 19:59:14 -0800

    Update dependencies

--- project.clj
+++ project.clj
@@ -6,12 +6,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
-                 [com.impossibl.pgjdbc-ng/pgjdbc-ng "0.5"]
-                 [com.zaxxer/HikariCP "2.3.8"]
+  :dependencies [[org.clojure/clojure "1.8.0-RC2"]
+                 [com.impossibl.pgjdbc-ng/pgjdbc-ng "0.6"]
+                 [com.zaxxer/HikariCP "2.4.2"]
 
 
                  ; for some reason this was required to remove
                  ; an log to console on each startup.
-                 [org.slf4j/slf4j-simple "1.7.10"]
+                 [org.slf4j/slf4j-simple "1.7.13"]
                  ])
