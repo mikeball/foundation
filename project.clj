245739ca8bf6acf5f45363e0a148871d026f9769
Mike Ball <michaelhball@gmail.com>
2016-02-02 00:41:44 -0800

    Bump version and deps.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject org.taoclj/foundation "0.1.0"
+(defproject org.taoclj/foundation "0.1.1"
+
   :description "A clojure data access library for postgresql."
 
   :url "https://github.com/mikeball/foundation"
@@ -6,7 +7,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.impossibl.pgjdbc-ng/pgjdbc-ng "0.6"]
                  [com.zaxxer/HikariCP "2.4.3"]
                  [cheshire "5.5.0"]
@@ -14,5 +15,6 @@
                  ; for some reason this was required to remove
                  ; an log to console on each startup.
                  [org.slf4j/slf4j-simple "1.7.13"]
+                ]
 
-                 ])
+  )
