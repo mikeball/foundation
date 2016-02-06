d85c5d13c4e3c581e87e5a138fc66ae0adbd1c56
Mike Ball <michaelhball@gmail.com>
2016-02-06 00:41:02 -0800

    slf4j-simple no longer needed

--- project.clj
+++ project.clj
@@ -10,11 +10,5 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.impossibl.pgjdbc-ng/pgjdbc-ng "0.6"]
                  [com.zaxxer/HikariCP "2.4.3"]
-                 [cheshire "5.5.0"]
+                 [cheshire "5.5.0"]])
 
-                 ; for some reason this was required to remove
-                 ; an log to console on each startup.
-                 [org.slf4j/slf4j-simple "1.7.13"]
-                ]
-
-  )
